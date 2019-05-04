b9e2fda0748080a32695f861de66d562917a7218
Miraç  ÖZTÜRK <32125234+miracozturk17@users.noreply.github.com>
2019-05-04 12:16:29 +0300

    V4
    Tablo düzenlendi.
    Template eklendi.

--- OM.WebUI/Controllers/ProductController.cs
+++ OM.WebUI/Controllers/ProductController.cs
@@ -21,18 +21,17 @@ namespace OM.WebUI.Controllers
             return View();
         }
 
-        public ActionResult ProductTable(string aramametnicontroller)
+        public ActionResult ProductTable(string searchtextcontroller)
         {
             var products = _productService.ProductList();
 
-            if (string.IsNullOrEmpty(aramametnicontroller) == false)
+            if (string.IsNullOrEmpty(searchtextcontroller) == false)
             {
-                products = products.Where(p =>p.Name!=null && p.Name.ToLower().Contains(aramametnicontroller.ToLower())).ToList();
+                products = products.Where(p => p.Name != null && p.Name.ToLower().Contains(searchtextcontroller.ToLower())).ToList();
             }
 
             return PartialView(products);
         }
-
 
         [HttpGet]
         public ActionResult Create()
@@ -44,6 +43,32 @@ namespace OM.WebUI.Controllers
         public ActionResult Create(Product product)
         {
             _productService.ProductSave(product);
+
+            return RedirectToAction("ProductTable");
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var product = _productService.GetProduct(id);
+
+            return PartialView(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            _productService.UpdateProduct(product);
+
+            return RedirectToAction("ProductTable");
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            _productService.DeleteProduct(id);
 
             return RedirectToAction("ProductTable");
         }
